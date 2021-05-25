978404cc0d27ea00e44393e93adaa49cef725a8b
Sofiene BEN KHEMIS <sofianee.007@gmail.com>
2021-05-25 12:28:19 +0200

    Update unit tests

---
+++ src/test/java/com/dev/cicd/data/controller/DataControllerTest.java
@@ -0,0 +1,37 @@
+package com.dev.cicd.data.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DataControllerTest {
+
+    @Autowired
+    DataController dataController;
+
+    @Test
+    void health() {
+        assertEquals( "HEALTH CHECK OK!", dataController.healthCheck());
+    }
+
+    @Test
+    void version() {
+        assertEquals( "The actual version is 1.0.0", dataController.version());
+    }
+
+    @Test
+    void nationsLength() {
+        Integer nationsLength = dataController.getRandomNations().size();
+        assertEquals( 10, nationsLength);
+    }
+
+    @Test
+    void currenciesLength() {
+        Integer currenciesLength = dataController.getRandomCurrencies().size();
+        assertEquals(20, currenciesLength);
+    }
+
+}
